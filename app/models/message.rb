9b1cfb639829b5bf48cf786516718ad61f211c04
Nancy LeGrand <nancy.legrand@gmail.com>
2010-03-10 13:18:01 -0500

    Add message associations and named scopes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,29 @@
 class Message < ActiveRecord::Base
+  # Associations
+  belongs_to :user
+  
+  has_one :reply,
+          :class_name => "Message",
+          :foreign_key => "reply_to"
+          
+  # Named Scopes
+  named_scope :recent,             
+              {:order => 'created_at DESC', limit => 5}
+             
+              
+  named_scope :type_message
+              lambda {(priority) |
+              {:conditions => ['priority = ?' , priority]}
+              }
+              
+  named_scope :messages_for_user
+              lambda {(user) |
+              {:conditions => ['user_id = ?' , user]}
+              }  
+              
+ named_scope :messages_for_team
+              lambda {(team) |
+              {:conditions => ['user_id in (select id from users where team_id = ?)', team]}
+              }            
+  
 end
