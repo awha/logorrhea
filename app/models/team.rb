0f61ba77fd95e4eedbabd34edcf678c3e7b06421
unknown <nkathuria@.(none)>
2010-03-10 13:12:30 -0500

    Got nested models working

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,11 @@ class Team < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name
   
+  def with_extra_users(extra=2)
+    extra.times { users.build }
+    self
+  end
+  
   has_many :users
+  accepts_nested_attributes_for :users
 end
